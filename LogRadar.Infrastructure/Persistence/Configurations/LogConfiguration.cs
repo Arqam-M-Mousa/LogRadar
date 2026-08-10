6b5081e47109593f541d772d6236591e9e33f43b
Arqam Mousa <arqam.mousa@gmail.com>
2026-08-11 01:57:44 +0300

    Add Logs table config.

---
+++ LogRadar.Infrastructure/Persistence/Configurations/LogConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using LogRadar.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LogRadar.Infrastructure.Persistence.Configurations;
+
+public class LogConfiguration : IEntityTypeConfiguration<Log>
+{
+    public void Configure(EntityTypeBuilder<Log> builder)
+    {
+        builder.ToTable("log");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .ValueGeneratedOnAdd();
+
+        builder.Property(x => x.Timestamp)
+            .IsRequired();
+
+        builder.Property(x => x.Level)
+            .HasConversion<string>()
+            .HasMaxLength(5)
+            .IsRequired();
+
+        builder.Property(x => x.Service)
+            .IsRequired();
+
+        builder.Property(x => x.Message)
+            .IsRequired();
+
+        builder.Property(x => x.Attributes)
+            .HasColumnType("jsonb");
+
+        builder.HasIndex(x => new { x.Timestamp, x.Id })
+            .IsDescending();
+
+        builder.HasIndex(x => new { x.Service, x.Timestamp, x.Id })
+            .IsDescending(false, true, true);
+    }
+
+}
